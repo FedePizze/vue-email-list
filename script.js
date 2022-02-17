60b94eb9f1ecd24c66c549e1cfecd1fa38002dae
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-02-17 18:24:56 +0100

    script.js

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ const app = new Vue({
     data:{
 
         arrayMail: [],
-        aaa: ''
+        numeri: [],
+        aaa: false
+    },
+
+    methods: {
+
+        caricaEmail() {
+            for(let i = 0; i < 10; i++) {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((risultato) => {
+    
+                    console.log(risultato)
+                    this.arrayMail.push(risultato.data.response)
+                });
+    
+            }
+        },
+        
     },
 
     mounted() {
-
-        
-        for(let i = 0; i < 10; i++) {
-
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((risultato) => {
-
-                console.log(risultato)
-                this.arrayMail.push(risultato.data.response)
-            });
-
+            
+        for(let i = 1; i <= 100; i++) {
+            const aaa = {num: i}
+            this.numeri.push(aaa)
         }
         
     }
